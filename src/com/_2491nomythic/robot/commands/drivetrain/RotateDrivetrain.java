8ca125ee78756fd59be0d32bed30fa523ee1fb07
nomythic <nomythic@nomythic-Latitude-2>
2017-01-12 17:49:58 -0600

    Removed rotateDrivetrainTime and started fixing rotateDrivetrain.java

--- src/com/_2491nomythic/robot/commands/drivetrain/RotateDrivetrain.java
+++ src/com/_2491nomythic/robot/commands/drivetrain/RotateDrivetrain.java
@@ -1,7 +1,6 @@
 package com._2491nomythic.robot.commands.drivetrain;
 
 import com._2491nomythic.robot.commands.CommandBase;
-import com._2491nomythic.robot.settings.Constants;
 import com._2491nomythic.robot.subsystems.Drivetrain;
 
 import edu.wpi.first.wpilibj.Timer;
@@ -10,33 +9,35 @@ import edu.wpi.first.wpilibj.Timer;
  *
  */
 public class RotateDrivetrain extends CommandBase {
-	private Drivetrain drivetrain;
-	public double leftSpeed,rightSpeed;
-	public Timer timer;
+	Timer timer;
+	double speedAndDirection;
+	double rotateDuration;
 
-    public RotateDrivetrain(double leftSpeed, double rightSpeed) {
+    public RotateDrivetrain(double speed,double time) {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     	requires(drivetrain);
     	timer = new Timer();
-    	this.leftSpeed = leftSpeed;
-    	this.rightSpeed = rightSpeed;
+    	speedAndDirection = speed;
+    	rotateDuration = time;
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
     	timer.start();
     	timer.reset();
+    	
+
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	drivetrain.drive(leftSpeed, rightSpeed);
+    	
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return timer.get() > Constants.rotateDrivetrainTime;
+    	return timer.get() > rotateDuration;
     }
 
     // Called once after isFinished returns true
