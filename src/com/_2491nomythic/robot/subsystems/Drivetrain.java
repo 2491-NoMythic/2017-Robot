dfc8d90c90c0e1656f2b92a93f9314ca45c0e2c8
Adrian <awkirchner@gmail.com>
2017-01-12 17:11:00 -0600

    added encoder methods in drivetrain subsystem

--- src/com/_2491nomythic/robot/subsystems/Drivetrain.java
+++ src/com/_2491nomythic/robot/subsystems/Drivetrain.java
@@ -77,6 +77,87 @@ public class Drivetrain extends Subsystem {
 		drive(0);
 	}
 	
+	public void resetLeftEncoder() {
+		encoderLeft.reset();
+	}
+	
+	/**
+	 * Resets the right drive encoder value to 0
+	 */
+	public void resetRightEncoder() {
+		encoderRight.reset();
+	}
+	
+	/**
+	 * Resets the center drive encoder value to 0
+	 */
+	public void resetCenterEncoder() {
+		encoderCenter.reset();
+	}
+	
+	/**
+	 * @return The left drive encoder
+	 */
+	public Encoder getLeftEncoder() {
+		return encoderLeft;
+	}
+	
+	/**
+	 * @return the right drive encoder
+	 */
+	public Encoder getRightEncoder() {
+		return encoderRight;
+	}
+	
+	/**
+	 * @return the right drive encoder
+	 */
+	public Encoder getCenterEncoder() {
+		return encoderCenter;
+	}
+	
+	/**
+	 * @return The value of the left drive encoder
+	 */
+	public double getLeftEncoderDistance() {
+		return encoderLeft.getDistance();
+	}
+	
+	/**
+	 * @return The value of the right drive encoder
+	 */
+	public double getRightEncoderDistance() {
+		return encoderRight.getDistance();
+	}
+	
+	/**
+	 * @return The value of the center drive encoder
+	 */
+	public double getCenterEncoderDistance() {
+		return encoderCenter.getDistance();
+	}
+	
+	/**
+	 * @return The speed of the left motor in feet per second
+	 */
+	public double getLeftEncoderRate() {
+		return encoderLeft.getRate();
+	}
+	
+	/**
+	 * @return The speed of the right motor in feet per second
+	 */
+	public double getRightEncoderRate() {
+		return encoderRight.getRate();
+	}
+	
+	/**
+	 * @return The speed of the center motor in feet per second
+	 */
+	public double getCenterEncoderRate() {
+		return encoderCenter.getRate();
+	}
+	
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
