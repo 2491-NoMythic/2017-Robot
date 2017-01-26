b4a52a8e8c78a3a23c0a955724372b27a1466242
nomythic <nomythic@nomythic-Latitude-1>
2017-01-26 17:46:46 -0600

    This is meme
    I made drive straight yahy

--- src/com/_2491nomythic/robot/commands/drivetrain/DriveStraightToPosition.java
+++ src/com/_2491nomythic/robot/commands/drivetrain/DriveStraightToPosition.java
@@ -1,33 +1,39 @@
-package com._2491nomythic.robot.commands.autonomous;
+package com._2491nomythic.robot.commands.drivetrain;
 
 import com._2491nomythic.robot.commands.CommandBase;
-import com._2491nomythic.robot.settings.Constants;
+
+import edu.wpi.first.wpilibj.command.Command;
 
 /**
  *
  */
-public class DriveStraight extends CommandBase {
+public class DriveStraightToPosition extends CommandBase {
 	private double speed;
+	private double distance;
 
-    public DriveStraight(double speed) {
+    public DriveStraightToPosition(double speed, double distance) {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
+    	requires(drivetrain);
     	this.speed = speed;
+    	this.distance = distance;
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	
+    	drivetrain.resetLeftEncoder();
+    	drivetrain.drive(speed);
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	drivetrain.drive(speed);
+    	
+    	
+    	
     }
-
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return drivetrain.getRightEncoderDistance() >= Constants.driveStraightAutoDistance;
+		return drivetrain.getLeftEncoderDistance() >= distance;
     }
 
     // Called once after isFinished returns true
