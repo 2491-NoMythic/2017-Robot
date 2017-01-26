8202bc3ab2ddbbd0817aa9632f4b80d5a5e5df7f
Adrian <awkirchner@gmail.com>
2017-01-26 17:50:08 -0600

    added a useLinearAcceleration variable to Variables.java

--- src/com/_2491nomythic/robot/settings/Variables.java
+++ src/com/_2491nomythic/robot/settings/Variables.java
@@ -2,6 +2,8 @@ package com._2491nomythic.robot.settings;
 
 public class Variables {
 	
+	public static boolean useLinearAcceleration = true;
+	
 	//Shifting Constants
 	public static double shiftUpSpeed = 1000;
 	public static double shiftDownSpeed = 1000;
