aca8a41ffbb9fa3c0d52e8c5bd626d7d07019864
nomythic <nomythic@nomythic-Latitude-4>
2017-02-02 20:12:40 -0600

    Added a bunch of camera stuff, not entirely finished

---
+++ src/com/_2491nomythic/robot/settings/CameraException.java
@@ -0,0 +1,10 @@
+package com._2491nomythic.robot.settings;
+
+public class CameraException extends Exception {
+	private static final long serialVersionUID = 1L;
+	
+	public CameraException(String message) {
+		super(message);
+	}
+
+}
---
+++ src/com/_2491nomythic/robot/settings/CameraPacket.java
@@ -0,0 +1,9 @@
+package com._2491nomythic.robot.settings;
+
+public class CameraPacket {
+	public int cameraX;
+	public int cameraY;
+	public int cameraHeight;
+	public int cameraWidth;
+
+}
