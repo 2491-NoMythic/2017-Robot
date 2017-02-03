fbeed7c80ba5c4596e56ea047714fc3ccdf4f8b6
Thomas <thetardis1968@gmail.com>
2017-02-02 18:07:41 -0600

    Added three GearSlot autos
    CenterGearSlot is finished, Left and Right are almost finished, but
    still need measurements in degrees and feet.

---
+++ src/com/_2491nomythic/robot/commands/autonomous/CenterGearSlot.java
@@ -0,0 +1,51 @@
+package com._2491nomythic.robot.commands.autonomous;
+
+import com._2491nomythic.robot.commands.CommandBase;
+import com._2491nomythic.robot.commands.drivetrain.DriveStraightToPosition;
+import com._2491nomythic.robot.commands.gearslot.OpenAndEjectGearSlot;
+
+/**
+ *
+ */
+public class CenterGearSlot extends CommandBase {
+	private DriveStraightToPosition firstDrive, secondDrive;
+	private OpenAndEjectGearSlot gearDeposit;
+
+    public CenterGearSlot() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	firstDrive = new DriveStraightToPosition(1, 7.7);
+    	secondDrive = new DriveStraightToPosition(-1, 1.5);
+    	gearDeposit = new OpenAndEjectGearSlot();
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	firstDrive.start();
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	if(!firstDrive.isRunning()) {
+    		gearDeposit.start();
+    		if(!gearDeposit.isRunning()) {
+    			secondDrive.start();
+    		}
+    	}	
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return !secondDrive.isRunning();
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	end();
+    }
+}
