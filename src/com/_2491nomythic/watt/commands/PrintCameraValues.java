2f8fb9a56eb5e7ee02aea7d518098edeaa7d31e2
nomythic <nomythic@nomythic-Latitude-4>
2017-02-07 19:52:03 -0600

    another camera change

--- src/com/_2491nomythic/watt/commands/PrintCameraValues.java
+++ src/com/_2491nomythic/watt/commands/PrintCameraValues.java
@@ -3,10 +3,13 @@ package com._2491nomythic.watt.commands;
 import com._2491nomythic.watt.settings.CameraException;
 import com._2491nomythic.watt.settings.CameraPacket;
 
+import edu.wpi.first.wpilibj.Timer;
+
 /**
  *
  */
 public class PrintCameraValues extends CommandBase {
+	private Timer timer;
 
     public PrintCameraValues() {
         // Use requires() here to declare subsystem dependencies
@@ -15,12 +18,8 @@ public class PrintCameraValues extends CommandBase {
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	try {
-			camera.readPacket();
-		} catch (CameraException e) {
-			e.printStackTrace();
-		}
-    	System.out.println("X:" + CameraPacket.cameraX + " Y:" + CameraPacket.cameraY + " Width:" + CameraPacket.cameraWidth + " Height" + CameraPacket.cameraHeight);
+    	timer.reset();
+    	timer.start();
     }
 
     // Called repeatedly when this Command is scheduled to run
@@ -29,11 +28,18 @@ public class PrintCameraValues extends CommandBase {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return timer.get() > 1;
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	timer.stop();
+    	try {
+			camera.readPacket();
+		} catch (CameraException e) {
+			e.printStackTrace();
+		}
+    	System.out.println("X:" + CameraPacket.cameraX + " Y:" + CameraPacket.cameraY + " Width:" + CameraPacket.cameraWidth + " Height" + CameraPacket.cameraHeight);
     }
 
     // Called when another command which requires one or more of the same
