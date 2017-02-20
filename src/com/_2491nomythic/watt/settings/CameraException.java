12e121f6b409df49e42bd190b468f5cea14bdb91
nomythic <nomythic@nomythic-Latitude-4>
2017-02-20 15:57:48 -0600

    i accidentally hit the commit button halfway through a commit

--- src/com/_2491nomythic/watt/settings/CameraException.java
+++ src/com/_2491nomythic/watt/settings/CameraException.java
@@ -1,6 +1,8 @@
 package com._2491nomythic.watt.settings;
 
 public class CameraException extends Exception {
+	private static final long serialVersionUID = -4107727669412268118L;
+
 	public CameraException(String message) {
 		super(message);
 	}
