0faf4aacefe0844c80e30527bd05b1d640cdbdef
nomythic <nomythic@nomythic-Latitude-1>
2017-02-21 16:25:19 -0600

    Made button a for combo auto gearslot

--- src/com/_2491nomythic/watt/OI.java
+++ src/com/_2491nomythic/watt/OI.java
@@ -6,6 +6,7 @@ import com._2491nomythic.watt.commands.drivetrain.DriveLock;
 import com._2491nomythic.watt.commands.drivetrain.ManualShift;
 import com._2491nomythic.watt.commands.drivetrain.NoTurnLock;
 import com._2491nomythic.watt.commands.drivetrain.RotateDrivetrainWithGyro;
+import com._2491nomythic.watt.commands.gearslot.OpenAndEjectGearSlot;
 import com._2491nomythic.watt.commands.gearslot.ToggleEjector;
 import com._2491nomythic.watt.commands.gearslot.ToggleGearSlot;
 import com._2491nomythic.watt.settings.Constants;
@@ -22,7 +23,7 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
 public class OI {
 
 	private final Joystick[] controllers = new Joystick[2];
-	Button driveLock, followObject, openDoors, ejectGear, climb, autoLeftTest, autoRightTest, autoCenterTest, drive1FootTest, shift, noTurnLock, rotateLeft, rotateRight, speedTest;
+	Button driveLock, followObject, openDoors, ejectGear, climb, autoLeftTest, autoRightTest, autoCenterTest, drive1FootTest, shift, noTurnLock, rotateLeft, rotateRight, speedTest,comboGear;
 	
 	public void init() {
 		controllers[0] = new Joystick(Constants.ControllerOnePort);
@@ -56,6 +57,8 @@ public class OI {
 		rotateRight = new JoystickPOVButton(controllers[ControllerMap.mainDriveController], ControllerMap.rotateDrivetrainRightPOV);
 		rotateRight.whenPressed(new RotateDrivetrainWithGyro(1, 47));
 		
+		comboGear = new JoystickPOVButton(controllers[ControllerMap.secondaryDriveController], ControllerMap.comboGearButton);
+		comboGear.whenPressed(new OpenAndEjectGearSlot());
 	}
 	
 	/**
