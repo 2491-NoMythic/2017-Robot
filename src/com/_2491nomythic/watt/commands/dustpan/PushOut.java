df7368e6b56792f186e4d138233b68ecb9fd6cd2
nomythic <nomythic@nomythic-Latitude-1>
2017-03-21 17:24:04 -0500

    This is some of the push out code

---
+++ src/com/_2491nomythic/watt/commands/dustpan/PushOut.java
@@ -0,0 +1,51 @@
+package com._2491nomythic.watt.commands.dustpan;
+
+import com._2491nomythic.watt.commands.CommandBase;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class PushOut extends CommandBase{
+Timer timer;
+
+    public PushOut() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(dustpan);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	dustpan.flipUp();
+    	timer.reset();
+    	timer.start();
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	dustpan.runMotor(1);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        if (timer.get() >= 5){
+        	return true;
+        }
+        else{
+        	return false;
+        }
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	dustpan.stop();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
