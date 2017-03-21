62300969b95fc4e3e2ee4f3875fb96bd288d3f7a
nomythic <nomythic@nomythic-Latitude-1>
2017-03-21 17:11:16 -0500

    Hey dog this in a roll in only command but it looks like rollin so thats a thing

---
+++ src/com/_2491nomythic/watt/commands/dustpan/RollIn.java
@@ -0,0 +1,42 @@
+package com._2491nomythic.watt.commands.dustpan;
+
+import com._2491nomythic.watt.commands.CommandBase;
+
+
+/**
+ *
+ */
+public class RollIn extends CommandBase {
+
+    public RollIn() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(dustpan);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize(){
+    	dustpan.flipDown();
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	dustpan.runMotor(1);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return dustpan.getLimitSwitch();
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	dustpan.stop();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	end();
+    }
+}
