9524849194ce67b52ae34b5f5a29b60ab3c051d3
Thomas <thetardis1968@gmail.com>
2017-03-22 14:20:34 -0500

    added a file for dustpan that lets you run the dustpan separately

---
+++ src/com/_2491nomythic/watt/commands/dustpan/RunIntake.java
@@ -0,0 +1,40 @@
+package com._2491nomythic.watt.commands.dustpan;
+
+import com._2491nomythic.watt.commands.CommandBase;
+
+/**
+ *
+ */
+public class RunIntake extends CommandBase {
+
+    public RunIntake() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(dustpan);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	dustpan.runMotor(1);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	dustpan.runMotor(0);
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	end();
+    }
+}
