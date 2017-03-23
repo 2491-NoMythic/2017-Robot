33fd2713a45b22c3d548579b936a6f8badef9a3b
Thomas <thetardis1968@gmail.com>
2017-03-22 20:49:34 -0500

    got bored, if something broke it was probably here
    just felt like writing code that will probably never get used

--- src/com/_2491nomythic/watt/commands/lights/ClimbFlash.java
+++ src/com/_2491nomythic/watt/commands/lights/ClimbFlash.java
@@ -7,35 +7,68 @@ import edu.wpi.first.wpilibj.Timer;
 /**
  *
  */
-public class LightsDefault extends CommandBase {
+public class ClimbFlash extends CommandBase {
 	private Timer timer;
-    public LightsDefault() {
+	private boolean climbTime;
+	private int state;
+
+    public ClimbFlash() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     	requires(lights);
+    	
     	timer = new Timer();
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	timer.reset();
     	timer.start();
-    	;
+    	timer.reset();
     	
+    	climbTime = false;
+    	
+    	lights.setColors(148, 0, 211);
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	lights.setColors(127,0,0);
+    	if(timer.get() > 105) {
+    		timer.reset();
+    		climbTime = true;
+    	}
+    	
+    	while(climbTime) {
+    		switch(state) {
+    		case 0:
+    			if(timer.get() >= 0.5) {
+    				lights.shutOffBlue();
+    				lights.shutOffGreen();
+    				lights.shutOffRed();
+    				state = 1;
+    			}
+    			break;
+    		case 1:
+    			if(timer.get() >= 1) {
+    				lights.setColors(148, 0, 211);
+    				timer.reset();
+    				state = 0;
+    			}
+    			break;
+    		default:
+    			System.out.println("Something went wrong in ClimbFlash. State: " + state);
+    		}
+    	}	
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return timer.get() >= 1000;
+        return false;
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	lights.shutOffBlue();
+    	lights.shutOffGreen();
     	lights.shutOffRed();
     }
 
