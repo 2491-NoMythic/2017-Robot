83816ad9fdbbc7f610b0b6e4e2b5cc6434d7c87a
Adrian <awkirchner@gmail.com>
2017-03-23 19:11:37 -0500

    javadoc for Climber.java

--- src/com/_2491nomythic/watt/subsystems/Climber.java
+++ src/com/_2491nomythic/watt/subsystems/Climber.java
@@ -5,7 +5,7 @@ import com.ctre.CANTalon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
- *
+ * The cylinder on the front of the robot that it uses to climb a rope
  */
 public class Climber extends Subsystem {
 	private CANTalon left, right;
@@ -19,23 +19,30 @@ public class Climber extends Subsystem {
 		return instance;
 	}
 	
+	/**
+	 * The cylinder on the front of the robot that it uses to climb a rope
+	 */
 	private Climber() {
 		left = new CANTalon(Constants.climberTalonLeftChannel);
 		right = new CANTalon(Constants.climberTalonRightChannel);
 	}
 	
+	/**
+	 * Runs the climber motors at a specified power
+	 * @param power The amount of power sent to the motors, from 0 to 1
+	 */
 	public void runClimberMotors(double power) {
 		left.set(Math.abs(power));
 		right.set(Math.abs(power));
 	}
 	
+	/**
+	 * Stops all movement of the climber motors
+	 */
 	public void stop() {
 		runClimberMotors(0);
 	}
     
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
-
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
