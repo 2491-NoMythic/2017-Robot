77beabcbf9bc5dad3143aefac63223b74d206ec7
Adrian <awkirchner@gmail.com>
2017-03-28 17:32:02 -0500

    javadoc for CameraException.java

--- src/com/_2491nomythic/watt/settings/CameraException.java
+++ src/com/_2491nomythic/watt/settings/CameraException.java
@@ -1,9 +1,16 @@
 package com._2491nomythic.watt.settings;
 
+/**
+ * An exception needed to run vision
+ */
 public class CameraException extends Exception {	
 
 	private static final long serialVersionUID = -4107727669412268118L; //auto-generated
 
+	/**
+	 * An exception needed to run vision
+	 * @param message The detail message for the exception
+	 */
 	public CameraException(String message) {
 		super(message);
 	}
