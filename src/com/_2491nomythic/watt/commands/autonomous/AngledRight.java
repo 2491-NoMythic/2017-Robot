23e974c21dea080abebbdf104f40bc806ad101ad
Adrian <awkirchner@gmail.com>
2017-04-01 09:21:59 -0500

    removed unused imports

--- src/com/_2491nomythic/watt/commands/autonomous/AngledRight.java
+++ src/com/_2491nomythic/watt/commands/autonomous/AngledRight.java
@@ -3,7 +3,6 @@ package com._2491nomythic.watt.commands.autonomous;
 import com._2491nomythic.watt.commands.CommandBase;
 import com._2491nomythic.watt.commands.drivetrain.DriveStraightToPosition;
 import com._2491nomythic.watt.commands.drivetrain.PivotFrontAUTOONLY;
-import com._2491nomythic.watt.commands.drivetrain.RotateDrivetrainWithGyro;
 import com._2491nomythic.watt.commands.drivetrain.RotateDrivetrainWithGyroPID;
 import com._2491nomythic.watt.commands.gearslot.OpenAndEjectGearSlot;
 import com._2491nomythic.watt.commands.gearslot.TogglePusher;
