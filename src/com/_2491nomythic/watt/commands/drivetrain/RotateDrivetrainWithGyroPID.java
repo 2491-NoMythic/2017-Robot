d06e058237ceb1c221cc9a63b8cc831004908c4c
Emilio Alvarez <e.l.alvarez@icloud.com>
2017-04-22 13:09:17 -0500

    Added absolute vs relative

--- src/com/_2491nomythic/watt/commands/drivetrain/RotateDrivetrainWithGyroPID.java
+++ src/com/_2491nomythic/watt/commands/drivetrain/RotateDrivetrainWithGyroPID.java
@@ -7,23 +7,30 @@ import com._2491nomythic.watt.commands.CommandBase;
  */
 public class RotateDrivetrainWithGyroPID extends CommandBase {
 	private double  target, relative;
+	private boolean type;
 
 	/**
 	 * Rotates the drivetrain to a specified angle using PID
 	 * @param angle The angle that the robot rotates to, where negative values rotate the robot counterclockwise
+	 * @param absolute Set true for absolute, false for relative 
 	 */
-    public RotateDrivetrainWithGyroPID(double angle) {
+    public RotateDrivetrainWithGyroPID(double angle, boolean absolute) {
         // Use requires() here to declare subsystem dependencies
       	requires(drivetrain);
       	target = angle;
-    	
+    	type = absolute;
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
     	relative = ((drivetrain.getGyroAngle() + target) % 360 + 360) % 360;
     	System.out.println(relative);
-    	drivetrain.setSetpoint(relative);
+    	if (type) { //absolute
+    		drivetrain.setSetpoint(target);
+    	}
+    	else { //relative
+    		drivetrain.setSetpoint(relative);
+    	}
     	drivetrain.enable();
     }
 
