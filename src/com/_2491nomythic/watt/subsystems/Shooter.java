a47fd5d02346e59a2d66e2928c4374afdd9d3c3a
Thomas <thetardis1968@gmail.com>
2017-05-03 14:11:37 -0500

    added an empty shooter subsystem

---
+++ src/com/_2491nomythic/watt/subsystems/Shooter.java
@@ -0,0 +1,22 @@
+package com._2491nomythic.watt.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Shooter extends Subsystem {
+
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+	
+	public Shooter() {
+
+	}
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
